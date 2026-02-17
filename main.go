d04e40fc43c3b4e945ea2ab6d0fefa644940ed74
pymk <pymk@users.noreply.github.com>
2026-02-16 16:58:35 -0800

    initial commit

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
-	_ "fmt"
-	_ "os"
-
-	_ "github.com/alecthomas/kong"
-	_ "github.com/go-janitor/cli"
+	"github.com/alecthomas/kong"
+	"github.com/pymk/go-janitor/cli"
 )
 
 func main() {
-	// Parse CLI arguments
-	// Execute CLI.Run()
-	// Handle errors and exit codes
+	var c cli.CLI
+
+	// Parse CLI arguments with kong
+	ctx := kong.Parse(&c,
+		kong.Name("janitor"),
+		kong.Description("String clean-up CLI tool for transforming stdin."),
+		kong.UsageOnError(),
+	)
+
+	// Execute the transformation pipeline
+	err := c.Run()
+
+	// Handle errors
+	ctx.FatalIfErrorf(err)
 }
